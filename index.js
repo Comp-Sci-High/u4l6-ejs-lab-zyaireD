a22f8e80b973814c772cb744d53e817383b12a6a
zyaireD <zyaire.dowe25@compscihigh.org>
2025-01-27 15:29:02 +0000

    alomst done

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const inventory = [
 ]
 
 // Task 1: Set the view engine to EJS. 
-
+app.set('view engine', 'ejs');
 
 
 app.use((req, res, next) => {
@@ -74,27 +74,34 @@ app.use((req, res, next) => {
 
 // Task 1: Set up the static middleware
 
-
+app.use(express.static(-dorname +"/public"))
 
 // Task 2: Set up the route handler for / to send back the index.html file
-
+app.get("/", (req, res)=>{
+  res.send("index.html", data);
+})
 
 
 // Task 3: Set up the route handler for /mens which sends back category.ejs with the men's array
-
+app.get("/mens", (req, res)=>{
+  res.render("category.ejs", inventory[0]);
+})
 
 
 // Task 4: Plug in the values in category.ejs to get the page working
-// PINK ONLY: Set up a route handler for /womens to pass in similar data for women's
+
 
 
 
 // Task 5: Set up the route handler for /item/0 which sends back the first item in product.ejs
+app.get("/item/0", (req, res)=>{
+  res.render("product.ejs", inventory[0]items[0]);
 
+})
 
 
 // Task 6: Plug in the values in product.ejs to get the page working
-// Extra credit: modify the /item/0 route handler to have dynamic path parameter and return any item's data
+
 
 app.listen(3000, () => {
   console.log("Server running")
